8813e1d86a2b7002f9ecd5e55e7640a1168b727e
zhiqiang <428367178@qq.com>
2019-07-29 17:45:12 +0800

    本地工程添加到git

---
+++ git-test-project/src/com/itheima/git/FristClass.java
@@ -0,0 +1,13 @@
+package com.itheima.git;
+
+public class FristClass {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+    }
+}
